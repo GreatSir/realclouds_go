b453bae397116d31ddae53fe3841d2af5a9faf9c
shibingli@yeah.net <shibingli@yeah.net>
2017-11-12 14:06:24 +0800

    init

---
+++ middleware/mysql.go
@@ -0,0 +1,92 @@
+package middleware
+
+import (
+	"fmt"
+	"log"
+	"sync"
+
+	"ayerdudu.com/utils"
+	"github.com/jinzhu/gorm"
+	_ "github.com/jinzhu/gorm/dialects/mysql" //Gorm 支持
+	"github.com/labstack/echo"
+)
+
+//MySQLConf MySQL config
+func MySQLConf() (*gorm.DB, error) {
+
+	devMode := utils.GetENVToBool("DEV_MODE")
+
+	dbHost := utils.GetENV("DB_HOST")
+	if len(dbHost) == 0 {
+		dbHost = "127.0.0.1:3306"
+	}
+
+	dbUserName := utils.GetENV("DB_USERNAME")
+	if len(dbUserName) == 0 {
+		dbUserName = "ayerdudu"
+	}
+
+	dbPassword := utils.GetENV("DB_PASSWORD")
+	if len(dbPassword) == 0 {
+		dbPassword = "AyerDudu888"
+	}
+
+	dbDataBase := utils.GetENV("DB_DATABASE")
+	if len(dbDataBase) == 0 {
+		dbDataBase = "ayerdudu"
+	}
+
+	dbMaxIdleConns, err := utils.GetENVToInt("DB_MAXIDLECONNS")
+	if nil != err {
+		dbMaxIdleConns = 10
+	}
+
+	dbMaxOpenConns, err := utils.GetENVToInt("DB_MAXOPENCONNS")
+	if nil != err {
+		dbMaxOpenConns = 100
+	}
+
+	dbURL := fmt.Sprintf("%s:%s@tcp(%s)/%s?charset=utf8mb4&collation=utf8mb4_unicode_ci&parseTime=True&loc=Asia%%2FShanghai&timeout=30s", dbUserName, dbPassword, dbHost, dbDataBase)
+
+	db, err := gorm.Open("mysql", dbURL)
+	if nil != err {
+		return nil, err
+	}
+
+	db.DB().SetMaxIdleConns(dbMaxIdleConns)
+	db.DB().SetMaxOpenConns(dbMaxOpenConns)
+
+	db.LogMode(devMode)
+
+	if err = db.DB().Ping(); nil != err {
+		return nil, err
+	}
+
+	return db, nil
+}
+
+//DB *
+type DB struct {
+	Gorm  *gorm.DB
+	Mutex sync.RWMutex
+}
+
+//MySQL New MySQL dirver
+func MySQL() *DB {
+	db, err := MySQLConf()
+	if nil != err {
+		log.Fatalf("New mysql driver error: %v", err.Error())
+		return nil
+	}
+	return &DB{Gorm: db}
+}
+
+//MwMySQL MySQL middleware
+func (d *DB) MwMySQL(next echo.HandlerFunc) echo.HandlerFunc {
+	return func(c echo.Context) error {
+		d.Mutex.Lock()
+		defer d.Mutex.Unlock()
+		c.Set("mysql", d.Gorm)
+		return next(c)
+	}
+}
---
+++ models/model.go
@@ -0,0 +1,83 @@
+package models
+
+import (
+	"time"
+
+	"github.com/jinzhu/gorm"
+	"github.com/labstack/echo"
+	"github.com/pborman/uuid"
+)
+
+//Model *
+type Model struct {
+	ID          string     `sql:"index" gorm:"primary_key;column:id;type:varchar(100)" json:"id,omitempty" xml:"id,omitempty"`
+	Name        string     `sql:"index" gorm:"column:name;type:varchar(100)" json:"name,omitempty" xml:"name,omitempty"`
+	Description string     `gorm:"column:description;type:text" json:"description,omitempty" xml:"description,omitempty"`
+	CreatedAt   time.Time  `sql:"index" gorm:"column:created_at;type:timestamp" json:"created_at,omitempty" xml:"created_at,omitempty"`
+	UpdatedAt   time.Time  `sql:"index" gorm:"column:updated_at;type:timestamp" json:"updated_at,omitempty" xml:"updated_at,omitempty"`
+	DeletedAt   *time.Time `sql:"index" gorm:"column:deleted_at;type:timestamp" json:"deleted_at,omitempty" xml:"deleted_at,omitempty"`
+	SortNumber  int        `gorm:"column:sort_number;type:int(11)" json:"sort_number,omitempty" xml:"sort_number,omitempty"`
+
+	MemcachedFlags            int   `gorm:"column:flags;type:int(11)" json:"flags,omitempty" xml:"flags,omitempty"`
+	MemcachedCasColumn        int64 `gorm:"column:cas_column;type:bigint(20)" json:"cas_column,omitempty" xml:"cas_column,omitempty"`
+	MemcachedExpireTimeColumn int   `gorm:"column:expire_time_column;int(11)" json:"expire_time_column,omitempty" xml:"expire_time_column,omitempty"`
+}
+
+//BeforeCreate ID处理
+func (d *Model) BeforeCreate(scope *gorm.Scope) error {
+	uuidStr := uuid.NewRandom().String()
+	if err := scope.SetColumn("ID", uuidStr); nil != err {
+		return err
+	}
+	return nil
+}
+
+//PicModel *
+type PicModel struct {
+	Pic    string `gorm:"column:pic;type:text" json:"pic,omitempty" xml:"pic,omitempty"`
+	Pic3rd string `gorm:"column:pic_3rd;type:text" json:"pic_3rd,omitempty" xml:"pic_3rd,omitempty"`
+}
+
+//ComputeOffset *
+func ComputeOffset(pageNumber, pageSize int) int {
+	offset := 0
+	if pageNumber > 1 {
+		offset = (pageNumber - 1) * pageSize
+	}
+	return offset
+}
+
+//DBCtx *
+type DBCtx struct {
+	WebContext echo.Context
+}
+
+//NewDBCtx *
+func NewDBCtx(c echo.Context) *DBCtx {
+	dbCtx := &DBCtx{WebContext: c}
+	return dbCtx
+}
+
+//MySQL *
+func (d *DBCtx) MySQL() *gorm.DB {
+	return d.WebContext.Get("mysql").(*gorm.DB)
+}
+
+//AutoMigrate * AutoMigrate
+func AutoMigrate(c echo.Context, schema ...interface{}) error {
+	return NewDBCtx(c).MySQL().AutoMigrate(schema...).Error
+}
+
+//ParamsToMaps *
+func ParamsToMaps(args []string) map[string]string {
+
+	params := make(map[string]string)
+
+	for i := 0; i < len(args); i = i + 2 {
+		key := args[i]
+		val := args[i+1]
+		params[key] = val
+	}
+
+	return params
+}
