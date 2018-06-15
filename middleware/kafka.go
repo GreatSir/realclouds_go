42dc27601e8c4fc9df8e1b8b9d743c937024cebe
shibingli@yeah.net <shibingli@yeah.net>
2018-06-15 17:30:13 +0800

    Update

--- middleware/kafka.go
+++ middleware/kafka.go
@@ -177,7 +177,7 @@ func (k *Kafka) Subscription(topics []string, group string,
 	defer consumer.Close()
 
 	signals := make(chan os.Signal, 1)
-	signal.Notify(signals, os.Interrupt)
+	signal.Notify(signals, os.Interrupt, os.Kill)
 
 	go func() {
 		for err := range consumer.Errors() {
